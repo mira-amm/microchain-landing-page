13b3a370c603bd1b2ca4047b9fce705ab4db68c5
Charly Martin <charly@hey.com>
2022-08-08 14:33:18 +0100

    Add style

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        mcs: {
+          beige: { 100: "#EEECE7" },
+          black: { DEFAULT: "#000000" },
+          blue: { DEFAULT: "#5179F4" },
+          yellow: { DEFAULT: "#FFC501" },
+          red: { DEFAULT: "#FF3C5C" },
+          green: { DEFAULT: "#01EC97" },
+        },
+      },
+    },
   },
   plugins: [],
 };
